ae48a6dcd2ee7c78e90bfa5c1f7598113d80baba
James Leigh <james@3roundstones.com>
2015-11-18 16:43:09 -0500

    Add 0035_GraphNode_Basic

--- components/RequestTemplate.js
+++ components/RequestTemplate.js
@@ -5,6 +5,7 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var uriTemplates = require('uri-templates');
+var Handlebars = require('handlebars');
 var noflo = require('noflo');
 
 exports.getComponent = function() {
@@ -32,7 +33,7 @@ exports.getComponent = function() {
                 required: false,
                 addressable: false,
                 buffered: false,
-                process: ondata(assignTemplate('method'))
+                process: on({data: assignUriTemplate('method')})
             },
             url: {
                 description: "URI-Template (RFC6570)",
@@ -40,31 +41,39 @@ exports.getComponent = function() {
                 required: true,
                 addressable: false,
                 buffered: false,
-                process: ondata(assignTemplate('url'))
+                process: on({data: assignUriTemplate('url')})
             },
             headers: {
-                description: "Request headers",
-                datatype: 'string',
+                description: "Request headers as name:template pairs using the URI-Template syntax",
+                datatype: 'object',
                 required: false,
                 addressable: false,
                 buffered: false,
-                process: ondata(assignTemplate('headers'))
+                process: on({data: assignUriTemplate('headers')})
             },
             body: {
-                description: "Request body template",
+                description: "Request body template using the Handlebars syntax",
                 datatype: 'string',
                 required: false,
                 addressable: false,
                 buffered: false,
-                process: ondata(assignTemplate('body'))
+                process: on({data: assignHandlebarsTemplate('body')})
             },
-            'in': {
-                description: "Object data used to populate the templates",
+            data: {
+                description: "Object data used to populate the templates, but not trigger the request",
                 datatype: 'object',
                 required: true,
                 addressable: false,
                 buffered: false,
-                process: ondata(received)
+                process: on({data: merge('base')})
+            },
+            'in': {
+                description: "Object data used to populate the templates and trigger the request",
+                datatype: 'object',
+                required: true,
+                addressable: false,
+                buffered: false,
+                process: on({data: assign('data'), disconnect: execute})
             }
         }
     }), {
@@ -74,40 +83,69 @@ exports.getComponent = function() {
     });
 };
 
-function ondata(callback) {
-	return function(event, payload) {
-	    switch(event) {
-		    case 'data': return callback.call(this.nodeInstance, payload);
-		};
+function on(type, callback) {
+    return function(event, payload) {
+        if (type[event]) type[event].call(this.nodeInstance, payload);
+    };
+}
+
+function assign(name){
+    return function(data){
+        this[name] = data;
+    };
+}
+
+function merge(name){
+    return function(data){
+        this[name] = _.extend(this[name] || {}, data);
+    };
+}
+
+function assignUriTemplate(name) {
+	return function(template){
+	    if (_.isString(template)) {
+	        var compiled = new uriTemplates(template);
+		    this[name] = function(data){
+		        return compiled.fill(data);
+		    };
+	    } else if (_.isObject(template)) {
+	        var compiled = _.mapObject(template, function(temp){
+	            return new uriTemplates(temp);
+	        });
+		    this[name] = function(data){
+		        return _.mapObject(compiled, function(comp){
+		            return comp.fill(data);
+		        });
+		    };
+	    } else {
+		    this[name] = function(data){
+		        return template;
+		    };
+        }
 	};
 }
 
-function assignTemplate(name) {
-	return function(data){
-		this[name] = compileTemplate(data);
+function assignHandlebarsTemplate(name) {
+	return function(template){
+		this[name] = Handlebars.compile(template);
 	};
 }
 
-function compileTemplate(template) {
-	if (_.isString(template)){
-		return new uriTemplates(template);
-	} else if (_.isObject(template)) {
-		return _.object(_.keys(template), _.values(template).map(compileTemplate));
-	} else {
-		return template;
-	}
-}
-
-function received(data) {
+function execute() {
+	if (!this.url) return _.defer(execute.bind(this));
 	var self = this;
-	var options = url.parse(self.url.fill(data));
+	var data = self.base && self.data ? _.extend({}, self.base, self.data) :
+	    self.base ? self.base : self.data;
+	var options = url.parse(self.url(data));
 	var prot = options.protocol == 'https:' ? https : http;
-	var req = prot.request(_.extend(options, self.headers, {
-		method: self.method ? self.method.fill(data) : 'GET'
+	var req = prot.request(_.extend(options, {
+		method: self.method ? self.method(data) : 'GET',
+		headers: self.headers(data)
 	}), function(res){
 		var output = res.statusCode < 400 ?
 				self.outPorts.out : self.outPorts.error;
 		res.setEncoding('utf8');
+	    output.connect();
 		res.on('data', function(data){
 			output.send(data);
 		}).on('end', function(){
@@ -120,6 +158,6 @@ function received(data) {
 		self.outPorts.error.send(e);
 		self.outPorts.error.disconnect();
 	});
-	if (self.body) req.write(self.body.fill(data));
+	if (self.body) req.write(self.body(data));
 	req.end();
 }
