78c3d1afc8999d92d7979ea648dda5b41ec82fc6
James Leigh <james@3roundstones.com>
2016-01-11 15:19:10 -0500

    Remove group support in round-robin (parallel and nested groups weren't supported anyway)

--- components/round-robin.js
+++ components/round-robin.js
@@ -9,62 +9,36 @@ exports.getComponent = function() {
     return _.extend(new noflo.Component({
         outPorts: {
             out: {
-                description: "Only one socket will receive each group of data packets",
+                description: "Only one socket will receive each data packet",
                 datatype: 'all'
             }
         },
         inPorts: {
             'in': {
                 description: "Each input group is send to exactly one output socket",
+                addressable: true,
                 datatype: 'all',
                 required: true,
-                process: basenode.on({
-                    begingroup: begingroup,
-                    data: send,
-                    endgroup: endgroup
-                })
+                process: basenode.on({data: send})
             }
         }
     }), {
-        description: "Every group of data packet will be sent to at most one out-port socket",
+        description: "Every data packet will be sent to at most one out-port socket",
         icon: 'refresh'
     });
 };
 
-function begingroup(group){
-    this.groupPort = nextSocket(this);
-    onceConnected(this.groupPort, function(socket){
-        socket.beginGroup(group);
-    });
-}
-
-function send(data) {
-    var disconnect = !this.groupPort;
-    onceConnected(this.groupPort || nextSocket(this), function(socket){
+function send(data, socketIndex) {
+    nextSocket(this).then(function(socket){
         socket.send(data);
-        if (disconnect) {
-            socket.disconnect();
-        }
-    });
-}
-
-function endgroup() {
-    onceConnected(this.groupPort, function(socket){
-        socket.endGroup();
         socket.disconnect();
     });
-    this.groupPort = null;
-}
-
-function onceConnected(socket, callback) {
-    if (!socket) return;
-    else if (socket.isConnected()) return callback(socket);
-    socket.once('connect', callback.bind(this, socket));
-    socket.connect();
 }
 
 function nextSocket(self) {
     var sockets = self.outPorts.out.sockets;
-    self.socketId = (_.isFinite(self.socketId) ? 1 + self.socketId : 0) % sockets.length;
-    return sockets[self.socketId];
+    self.socketId = (_.isFinite(self.socketId) ? ++self.socketId : 0) % sockets.length;
+    var socket = sockets[self.socketId];
+    if (socket) return Promise.resolve(socket);
+    else return Promise.reject(Error("No attached output sockets"));
 }
