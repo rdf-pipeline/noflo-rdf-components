43769bfc4d3799a7402f4cfb64526b44b1f26e9d
glenna-m <glenna.hokukahu@gmail.com>
2016-01-28 23:10:35 -0800

    Support for serialization/deserialization and standard Javascript object comparisons

--- components/lm.js
+++ components/lm.js
@@ -1,38 +1,85 @@
 /**
  * lm.js
  *
- * This file contains the code used to create an lm object.
+ * This file contains the code used to create, serialize, deserialize, and compare
+ * a last modified (LM) object. For more information about RDF pipeline LMs, please 
+ * refer to https://github.com/rdf-pipeline/framework/wiki/LM
  */
+
+var _ = require('underscore');
 
 globalLm = { timestamp: Date.now(),  
              counter: 0 };
 
-module.exports = {
+/**
+ * Get a special last modified timestamp that will have the millicseconds
+ * since 1970 plus a counter that guarantees uniqueness on each call, even
+ * if the number of milliseconds has not changed (counter will increment to
+ * force uniqueness.
+ *
+ * @param string optional string LM
+ *
+ * @return new LM is no lm string or a deserialized Lm built from the 
+ *         string if a good Lm string was given
+ */
+function Lm( string ) { 
 
-  /**
-   * Get a special last modified timestamp that will have the millicseconds
-   * since 1970 plus a counter that guarantees uniqueness on each call, even
-   * if the number of milliseconds has not changed (counter will increment to
-   * force uniqueness.
-   */
-  Lm: function() {
+   // If we get an LM string, parse & validate it
+   var components = (string) ? arguments[0].match(/LM(\d+)\.(\d+)/) : null;
+   if ( components && 
+      (( ! _.isArray( components ) ) || components.length !== 3)) { 
+      throw new Error('Invalid Lm format on argument ',arguments[0]);
+   }
+  
+   if ( components ) { 
+      // Construct LM from the string components
+      this.timestamp = Number( components[1] );
+      this.counter = Number( components[2] );
 
-    var lm = { timestamp: Date.now(),  
-               counter: 0 };
+   } else { 
 
-    // Does my new lm have a different timestamp from the last one?
-    if (lm.timestamp === globalLm.timestamp) {
-      // Got identical timestamps - increment the counter
-      lm.counter = ++globalLm.counter;
-    } else {
-      // Different timestamps.  We can set the globalLm to the current Lm values
-      // so the next time we get called, we know what we last had
-      globalLm = { timestamp: lm.timestamp, 
-                   counter: 0};
+      // Construct LM from scratch 
+      this.timestamp = Date.now();
+      this.counter = 0;
+
+       // Does my new lm have a different timestamp from the last one?
+       if (this.timestamp === globalLm.timestamp) {
+           // Got identical timestamps - increment the counter
+           this.counter = ++globalLm.counter;
+       } else {
+          // Different timestamps.  We can set the globalLm to the current Lm values
+          // so the next time we get called, we know what we last had
+          globalLm = { timestamp: this.timestamp, 
+                       counter: 0};
+        }
+   }
+}
+
+/**
+ * The toString API for an LM converts the Javascript object representation to 
+ * a fixed-length string, such as: LM1328113669.00000000000000001
+ * where the number before the decimal point is the timestamp value, and
+ * the digits after the decimal point represent the counter.  
+ */
+var MAX_DIGITS = (Number.MAX_SAFE_INTEGER.toString().split('')).length;
+Lm.prototype.toString =  function() {
+    var counter = this.counter.toString();
+    return 'LM'+this.timestamp+'.'+Array(MAX_DIGITS-counter.length+1).join("0") + counter;
+}
+
+Lm.prototype.valueOf =  function() {
+    var counter = this.counter.toString();
+    return this.timestamp+'.'+Array(MAX_DIGITS-counter.length+1).join("0") + counter;
+}
+
+/**
+ * construct an LM either from scratch or from the specified LM string 
+ */
+module.exports = function( string ) {
+
+    if ( arguments.length > 1 ) { 
+        throw new Error('Lm constructor expects at most one Lm string argument!');
     }
-   
-    return lm;
-  }
 
+    return new Lm( string );
 };
-
--- test/lm-mocha.js
+++ test/lm-mocha.js
@@ -1,6 +1,6 @@
 /**
- * File: lm.js
- * Unit tests for the lm APIs defined in components/rdf-pipeline/lm.js
+ * File: Lm-mocha.js
+ * Unit tests for the lm APIs defined in components/rdf-pipeline/Lm-mocha.js
  */
 
 var chai = require('chai');
@@ -8,54 +8,117 @@ var expect = chai.expect;
 var should = chai.should();
 
 var sinon = require('sinon');
+var _ = require('underscore');
 
-var lm = require('../components/lm');
+var Lm = require('../components/Lm');
 
-describe('lm', function() {
+describe('Lm', function() {
 
-  it('should exist as an object', function() {
-    lm.should.exist;
-    lm.should.be.an('object');
-  });
-
-  describe('#Lm', function () {
-
-    it('should exist in module.exports', function() {
-      lm.Lm.should.exist;
-      lm.Lm.should.be.a('function');
+    it('should exist as a function', function() {
+        Lm.should.exist;
+        Lm.should.be.a('function');
     });
 
     it('should create a new lm object"', function() {
 
-      var currentDate = Date.now();
-      var myLm = lm.Lm();
+        var currentDate = Date.now();
+        var myLm = Lm();
 
-      // Does it have the right properties?
-      myLm.should.not.be.null;
-      myLm.should.be.an('object');
-      myLm.should.have.all.keys( 'timestamp', 'counter' );
-      myLm.counter.should.equal(0); 
-      myLm.timestamp.should.be.at.least( currentDate );
+        // Does it have the right properties?
+        myLm.should.not.be.null;
+        myLm.should.be.an('object');
+        myLm.constructor.name.should.equal('Lm');
+        myLm.should.have.all.keys( 'timestamp', 'counter' );
+        myLm.counter.should.equal(0); 
+        myLm.timestamp.should.be.at.least( currentDate );
     });
 
     it('should increment the counter when Lms have the same timestamp', function() {
-      var startingDate = Date.now();
-      sinon.stub(Date, 'now', function() {
-        return startingDate;
-      });
+        var startingDate = Date.now();
+        sinon.stub(Date, 'now', function() {
+            return startingDate;
+        });
 
-      var myLm1 = lm.Lm();
-      var myLm2 = lm.Lm();
-      var myLm3 = lm.Lm();
+        var myLm1 = Lm();
+        var myLm2 = Lm();
+        var myLm3 = Lm();
 
-      myLm1.timestamp.should.equal( myLm2.timestamp );
-      myLm2.timestamp.should.equal( myLm3.timestamp );
+        myLm1.timestamp.should.equal( myLm2.timestamp );
+        myLm2.timestamp.should.equal( myLm3.timestamp );
 
-      myLm1.counter.should.equal(0);
-      myLm2.counter.should.equal(1);
-      myLm3.counter.should.equal(2);
+        myLm1.counter.should.equal(0);
+        myLm2.counter.should.equal(1);
+        myLm3.counter.should.equal(2);
 
-      Date.now.restore();
+        Date.now.restore();
     });
-  });
+
+    it('should serialize with toString()', function() {
+
+        // LMs are composed of a timestamp and a counter in their native 
+        // Javascript object format.  However, they need to serialize to this 
+        // format: LM<timestamp>.<counter>  - this test verifies that we get
+        // this format on a toString() call with no value corruption
+
+        var myLm1 = Lm();
+        var myLm1String = myLm1.toString();
+
+        myLm1String.should.be.a('string');
+        myLm1String.startsWith('LM').should.be.true;
+        myLm1String.should.contain('.');
+
+        // Verify that the values we got in the string match the lm timestamp and counter
+        // First, extract timestamp and counter numbers from our string which will have
+        // look like  LM1454027961694.0000000000000000
+        var components = myLm1String.match(/LM(\d+)\.(\d+)/);
+        Number(components[1]).should.equal(myLm1.timestamp);
+        Number(components[2]).should.equal(myLm1.counter);
+    });
+
+    it('should deserialize with LM(<lm string>)', function() {
+
+        // Get a current LM and serialize it
+        var myLm1 = Lm();
+        var myLm1String = myLm1.toString();
+
+        // Deserialize the serialized LM and compare it with the original
+        var myLm2 = Lm( myLm1String );
+        myLm2.should.be.an('object');
+        myLm2.constructor.name.should.equal('Lm');
+        myLm2.timestamp.should.equal( myLm1.timestamp );
+        myLm2.counter.should.equal( myLm1.counter );
+
+        myLm2.should.not.equal(myLm1);  // should be different objects (so not equal)
+        myLm2.should.deep.equal(myLm1); // but with the same deep attribute values
+    });
+
+    it('should perform equality checks on same reference correctly', function() {
+
+        var myLm1 = Lm();
+        var myLm2 = myLm1;
+
+        ( myLm2 != myLm1 ).should.be.false;
+        ( myLm2 == myLm1 ).should.be.true;
+
+        ( myLm2.valueOf() != myLm1.valueOf() ).should.be.false;
+        ( myLm2.valueOf() == myLm1.valueOf() ).should.be.true;
+
+        ( myLm2 !== myLm1 ).should.be.false;
+        ( myLm2 === myLm1 ).should.be.true;
+
+        _.isEqual( myLm2, myLm1 ).should.be.true;
+    });
+
+    it('should handle objects with same property value equality checks consistently with other Javascript objects', function() {
+        
+        var myLm1 = Lm();
+        var myLm1String = myLm1.toString();
+        var myLm2 = Lm( myLm1String ); 
+
+
+        ( myLm2.valueOf() !== myLm1.valueOf() ).should.be.false;
+        ( myLm2.valueOf() === myLm1.valueOf() ).should.be.true;
+        
+         _.isEqual( myLm2, myLm1 ).should.be.true;
+    });
 });
