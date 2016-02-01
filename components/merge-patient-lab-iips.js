f199de722267c50852f9e6adad5d10dd33b15c57
James Leigh <james@3roundstones.com>
2016-02-01 16:47:55 -0500

    Add merge-patient-lab-iips-mocha.js

---
+++ components/merge-patient-lab-iips.js
@@ -0,0 +1,16 @@
+// merge-patient-lab-iips.js
+
+var noflo = require('noflo');
+
+exports.getComponent = function() {
+    return _.extend(new noflo.Component({
+        inPorts: {
+            patient: {
+            },
+            labwork: {
+            }
+        }
+    }), {
+        description: "Will merges a pair of patient and lab IIPs together into a single combined record."
+    });
+};
