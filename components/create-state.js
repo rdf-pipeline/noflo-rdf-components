3fd06cb00a8ca052c312d87083c70450b254af18
glenna-m <glenna.hokukahu@gmail.com>
2016-02-05 14:15:07 -0800

    Very simple State module and it's unit tests for review

---
+++ components/create-state.js
@@ -0,0 +1,30 @@
+/**
+ *  file: create-state.js
+ */
+
+var _ = require('underscore');
+
+var Lm = require('./Lm');
+
+/**
+ * Constructs and returns a new State object
+ *
+ * @param vnid virtual node id
+ * @param data the data to be stored in the state object.
+ * @param lm (optional) the last modified timestamp (Lm) for the object. 
+ *           See https://github.com/rdf-pipeline/framework/wiki/LM for more details.
+ *           If the lm is undefined, a new lm will be generated.
+ *
+ * @return the newly created state object.
+ */ 
+module.exports = function( vnid, data, lm ) { 
+
+       if ( _.isUndefined( vnid) ) {
+           throw new Error("Unable to create state because no vnid was provided");
+       }
+
+       var stateLm = lm || Lm();
+       return { vnid: vnid,
+                data: data, 
+                lm: stateLm };
+};
