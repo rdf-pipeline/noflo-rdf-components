9c0aa1a19ac6b35ae0f8feb786376211ab9d4f5f
James Leigh <james@3roundstones.com>
2016-02-09 16:53:24 -0500

    Use common-test

--- test/promise-output-mocha.js
+++ test/promise-output-mocha.js
@@ -9,6 +9,7 @@ var _ = require('underscore');
 var noflo = require('noflo');
 var componentFactory = require('../src/event-component-factory');
 var promiseOutput = require('../src/promise-output');
+var test = require('./common-test');
 
 describe('promise-output', function() {
     it("should resolve ondata function", function() {
@@ -19,8 +20,37 @@ describe('promise-output', function() {
                 })
             }},
             outPorts: promiseOutput.outPorts
-        }).then(componentFactory).then(createComponent).then(function(component){
-            return onceData(component, 'input', "hello");
+        }).then(componentFactory).then(test.createComponent).then(function(component){
+            return new Promise(function(resolve, reject) {
+                var output = noflo.internalSocket.createSocket();
+                var error = noflo.internalSocket.createSocket();
+                component.outPorts.output.attach(output);
+                component.outPorts.error.attach(error);
+                output.once('data', resolve);
+                error.once('data', reject);
+                test.sendData(component, 'input', "hello");
+            });
+        }).should.become("hello world");
+    });
+    it("should resolve second ondata function call", function() {
+        return Promise.resolve({
+            inPorts:{input:{
+                ondata: promiseOutput(function(payload){
+                    if ("hello" == payload) return payload + " world";
+                })
+            }},
+            outPorts: promiseOutput.outPorts
+        }).then(componentFactory).then(test.createComponent).then(function(component){
+            return new Promise(function(resolve, reject) {
+                var output = noflo.internalSocket.createSocket();
+                var error = noflo.internalSocket.createSocket();
+                component.outPorts.output.attach(output);
+                component.outPorts.error.attach(error);
+                output.once('data', resolve);
+                error.once('data', reject);
+                test.sendData(component, 'input', "poke");
+                test.sendData(component, 'input', "hello");
+            });
         }).should.become("hello world");
     });
     it("should send multiple packets on array result for ondata function", function() {
@@ -31,14 +61,14 @@ describe('promise-output', function() {
                 })
             }},
             outPorts: promiseOutput.outPorts
-        }).then(componentFactory).then(createComponent).then(function(component){
+        }).then(componentFactory).then(test.createComponent).then(function(component){
             return new Promise(function(resolve) {
                 var output = noflo.internalSocket.createSocket();
                 component.outPorts.output.attach(output);
                 output.once('data', function(){
                     output.once('data', resolve);
                 });
-                sendData(component, 'input', "hello");
+                test.sendData(component, 'input', "hello");
             });
         }).should.become("hello world");
     });
@@ -50,8 +80,16 @@ describe('promise-output', function() {
                 })
             }},
             outPorts: promiseOutput.outPorts
-        }).then(componentFactory).then(createComponent).then(function(component){
-            return onceData(component, 'input', "hello");
+        }).then(componentFactory).then(test.createComponent).then(function(component){
+            return new Promise(function(resolve, reject) {
+                var output = noflo.internalSocket.createSocket();
+                var error = noflo.internalSocket.createSocket();
+                component.outPorts.output.attach(output);
+                component.outPorts.error.attach(error);
+                output.once('data', resolve);
+                error.once('data', reject);
+                test.sendData(component, 'input', "hello");
+            });
         }).should.be.rejectedWith("hello world");
     });
     it("should log rejections to console.error when no reject port attached", function() {
@@ -62,14 +100,14 @@ describe('promise-output', function() {
                 })
             }},
             outPorts: promiseOutput.outPorts
-        }).then(componentFactory).then(createComponent).then(function(component){
+        }).then(componentFactory).then(test.createComponent).then(function(component){
             var console_error = console.error;
             return new Promise(function(resolve, reject) {
                 console.error = reject;
                 var output = noflo.internalSocket.createSocket();
                 component.outPorts.output.attach(output);
                 output.once('data', resolve);
-                sendData(component, 'input', "hello");
+                test.sendData(component, 'input', "hello");
             }).then(function(resolved) {
                 console.error = console_error;
                 return resolved;
@@ -79,34 +117,4 @@ describe('promise-output', function() {
             });
         }).should.be.rejectedWith("hello world");
     });
-    function createComponent(getComponent) {
-        var component = getComponent();
-        _.forEach(component.inPorts, function(port, name) {
-            port.nodeInstance = component;
-            port.name = name;
-        });
-        _.forEach(component.outPorts, function(port, name) {
-            port.nodeInstance = component;
-            port.name = name;
-        });
-        return component;
-    }
-    function sendData(component, port, payload) {
-        var socket = noflo.internalSocket.createSocket();
-        component.inPorts[port].attach(socket);
-        socket.send(payload);
-        socket.disconnect();
-        component.inPorts[port].detach(socket);
-    }
-    function onceData(component, sendPort, sendPayload) {
-        return new Promise(function(resolve, reject) {
-            var output = noflo.internalSocket.createSocket();
-            var error = noflo.internalSocket.createSocket();
-            component.outPorts.output.attach(output);
-            component.outPorts.error.attach(error);
-            output.once('data', resolve);
-            error.once('data', reject);
-            sendData(component, sendPort, sendPayload);
-        });
-    }
 });
