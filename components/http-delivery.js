21cbc7523be998b926fea95dbc9a733b674cdfe4
James Leigh <james@3roundstones.com>
2016-04-29 11:39:16 -0400

    Rename input port to request

--- components/http-delivery.js
+++ components/http-delivery.js
@@ -6,31 +6,31 @@ var wrapper = require('../src/javascript-wrapper.js');
 
 /**
  * This updater takes response content, response type, and a {req, res} pair as
- * input. It then writes the content and type to the response and returns the
- * input pair.
+ * request. It then writes the content and type to the response and returns the
+ * request pair.
  */
-module.exports = wrapper(function(content, type, input) {
+module.exports = wrapper(function(content, type, request) {
     if (_.isArray(content)) {
-        input.res.writeHead(200, 'OK', {
+        request.res.writeHead(200, 'OK', {
             'Content-Type': type || 'text/plain',
             'Content-Length': _.pluck(content, 'length').reduce(add, 0)
         });
-        content.forEach(input.res.write.bind(input.res));
+        content.forEach(request.res.write.bind(request.res));
     } else if (_.isString(content)) {
-        input.res.writeHead(200, 'OK', {
+        request.res.writeHead(200, 'OK', {
             'Content-Type': type || 'text/plain',
             'Content-Length': content.length
         });
-        input.res.write(content);
+        request.res.write(content);
     } else {
         var json = JSON.stringify(content);
-        input.res.writeHead(200, 'OK', {
+        request.res.writeHead(200, 'OK', {
             'Content-Type': type || 'application/json',
             'Content-Length': json.length
         });
-        input.res.write(json);
+        request.res.write(json);
     }
-    return input;
+    return request;
 });
 
 function add(a, b) {
