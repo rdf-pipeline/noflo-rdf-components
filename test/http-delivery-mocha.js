074bef476c44c8881f266da9f7b478b6bb552758
James Leigh <james@3roundstones.com>
2016-05-03 17:27:23 -0400

    Add SPARQL LOAD test for local endpoint

--- test/http-delivery-mocha.js
+++ test/http-delivery-mocha.js
@@ -6,15 +6,19 @@ chai.should();
 chai.use(chaiAsPromised);
 
 var http = require('http');
+var os = require('os');
+var path = require('path');
 var _ = require('underscore');
 var noflo = require('noflo');
 var test = require('./common-test');
+var stubs = require('./common-stubs');
 var httpDelivery = require('../components/http-delivery');
 
 describe('http-delivery', function() {
     var port = 1337;
     var origin = 'http://localhost:' + port;
-    var endpoint = "http://10.255.241.10:10035/repositories/http-delivery-mocha";
+    var endpoint = "http://localhost:8080/openrdf-sesame/repositories/http-delivery-mocha";
+    var endpoint_update = "http://localhost:8080/openrdf-sesame/repositories/http-delivery-mocha/statements";
     it("should receive a request using a sub-graph", function() {
         return new Promise(function(done, fail) {
             test.createNetwork({
@@ -67,5 +71,42 @@ describe('http-delivery', function() {
                 res.on('data', done);
             }).catch(fail);
         }).should.become("Hello There!");
+    });
+    xit("should round trip jsonld through SPARQL LOAD", function() {
+        this.timeout(10000);
+        if (!process.env['rdf-auth-file']) {
+            process.env['rdf-auth-file'] = path.join(os.tmpdir(), 'rdf-auth');
+        }
+        var john = {
+            "@context": "http://json-ld.org/contexts/person.jsonld",
+            "@graph": [{
+                "@id": "http://dbpedia.org/resource/John_Lennon",
+                "name": "John Lennon",
+                "spouse": "http://dbpedia.org/resource/Cynthia_Lennon"
+            }]
+        };
+        return test.createNetwork({
+            sparql: "rdf-components/rdf-sparql-clear-load-jsonld",
+            construct: "rdf-components/rdf-construct-jsonld"
+        }).then(function(network){
+            network.graph.addInitial('rdf-auth-file', 'sparql', 'auth_file_env');
+            network.graph.addInitial('rdf-auth-file', 'construct', 'auth_file_env');
+            network.graph.addInitial(endpoint_update, 'sparql', 'sparql_endpoint');
+            network.graph.addInitial(endpoint, 'construct', 'sparql_endpoint');
+            network.graph.addInitial(port, 'sparql', 'listen');
+            network.graph.addInitial(origin + '/', 'sparql', 'base_url');
+            return stubs.promiseLater().then(function(){
+                return new Promise(function(done) {
+                    test.onOutPortData(network.processes.sparql.component, 'output', done);
+                    network.graph.addInitial("urn:test:graph", 'sparql', 'target_graph_uri');
+                    network.graph.addInitial(john, 'sparql', 'parsed_jsonld');
+                });
+            }).then(stubs.promiseLater).then(function(){
+                return new Promise(function(done) {
+                    test.onOutPortData(network.processes.construct.component, 'output', done);
+                    network.graph.addInitial("urn:test:graph", 'construct', 'source_graph_uri');
+                });
+            });
+        }).should.eventually.have.property('data').that.has.property('@id', "http://dbpedia.org/resource/John_Lennon");
     });
 });
