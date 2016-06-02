8964f9d7ffd53c245a66022038c48f3e87bf1e03
James Leigh <james@3roundstones.com>
2016-06-02 16:34:58 -0400

    Change object component to support arrays

--- components/object.js
+++ components/object.js
@@ -5,11 +5,15 @@ var _ = require('underscore');
 var wrapper = require('../src/javascript-wrapper.js');
 
 /**
- * Creates a object from a key and value
+ * Creates a object from a key and value or array of keys and array of values
  * @param key Property key
  * @param value Property value
  */
 module.exports = wrapper(function(key, value) {
-    if (!_.isEmpty(key) && !_.isUndefined(value))
+    if (_.isEmpty(key) || _.isUndefined(value))
+        return;
+    else if (_.isArray(key) && _.isArray(value))
+        return _.object(key, value);
+    else
         return _.object([key], [value]);
 });
