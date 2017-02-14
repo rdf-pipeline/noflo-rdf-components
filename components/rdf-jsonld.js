708c319e9dc5c783b75224ccfda5ef0dd9a3878f
glenna-m <glenna.hokukahu@gmail.com>
2017-02-13 19:56:23 -0800

    rdf-jsonld component using new improved shared ttl-jsonld library Added support for framing to rdf-construct-jsonld graph Made testing for ref-construct more robust.

--- components/rdf-jsonld.js
+++ components/rdf-jsonld.js
@@ -1,117 +1,33 @@
 // rdf-jsonld.js
 
 var _ = require('underscore');
-var Promise = require('promise');
+
 var jsonld = require('jsonld').promises;
 
+var dataUtils = require('./lib/data-utils');
+var ttl2JsonLd = require('translators/lib/ttl-jsonld');
 var wrapper = require('../src/javascript-wrapper.js');
 
 /**
- * Converts the given graph object into a JSON LD Graph object using an optional
- * frame on the corresponding Component.
- * @param input RDF JS Interface Graph object
- * @param frame JSON-LD Frame object
+ * Converts the RDF graph input object into a JSON LD Graph object using an optional
+ * frame on the corresponding component.
+ *
+ * @param input (required) RDF JS Interface Graph object
+ * @param frame (optional) JSON-LD Frame object
+ * @param filter_bnode_attrs (optional) an array with the attribute names that should be filtered if they
+ *        hold a blank node name that is referenced only once.  Used when doing ShEx 
+ *        translations of fields that would not be accessible after JSON-LD conversion to RDF
+ *        such as id or _id.
+ *
  * @see http://json-ld.org/spec/latest/json-ld-framing/
  */
-module.exports = wrapper(function execute(input, frame) {
-    return buildJSON(input).then(function(json) {
-        if (frame) return jsonld.frame(json, frame);
-        else if (json.length == 1) return json[0];
-        else return json;
-    });
-});
+module.exports = wrapper(rdfToJsonLd); 
 
-/**
- * Promise of JSON-LD array of objects for the given graph.
- * @param graph RDF JS Interface Graph object
- */
-function buildJSON(graph) {
-    return new Promise(function(resolve) {
-        var jsonGraph = []
-        var subject = indexedSubject.bind(this, {}, jsonGraph);
-        graph.forEach(function(triple) {
-            var subj = subject(triple.subject);
-            var pred = triple.predicate.nominalValue;
-            var obj = objectValue(triple.object);
-            pushTriple(subj, pred, obj);
-        });
-        resolve(jsonGraph);
-    });
-}
+function rdfToJsonLd(input, frame, filter_bnode_attrs) {
+    var parsedInput = dataUtils.parseData(input, "Rdf-Jsonld component", "input"); 
+ 
+    var parsedFrame = _.isUndefined(frame) ? frame : dataUtils.parseData(frame, "Rdf-Jsonld component", "frame");
+    var parsedAttrs = _.isUndefined(filter_bnode_attrs) ? filter_bnode_attrs :  dataUtils.parseData(filter_bnode_attrs, "Rdf-Jsonld component", "filter bNode attributes");
 
-/**
- * Returns a JSON-LD object for the given subject using existing subjects were
- * possible.
- * @param subjects hash of existing subjects by their nominal value to their position in jsonGraph
- * @param jsonGraph array of existing subject objects
- * @param subject an RDF term implementing RDF JS Interface
- */
-function indexedSubject(subjects, jsonGraph, subject) {
-    var value = subject.nominalValue;
-    if (typeof subjects[value] === 'undefined') {
-        if (subject.interfaceName === 'BlankNode') {
-            jsonGraph.push({
-                '@id': subject.toString()
-            });
-        } else {
-            jsonGraph.push({
-                '@id': value
-            });
-        }
-        subjects[value] = jsonGraph.length - 1;
-    }
-    return jsonGraph[subjects[value]];
-}
-
-/**
- * Converts the RDF term into a JSON-LD object
- * @param object an RDF term implementing RDF JS Interface
- */
-function objectValue(object) {
-    var value = object.nominalValue;
-    if (object.interfaceName === 'NamedNode') {
-        return {
-            '@id': value
-        };
-    } else if (object.interfaceName === 'BlankNode') {
-        return {
-            '@id': object.toString()
-        };
-    } else if (object.language) {
-        return {
-            '@language': object.language,
-            '@value': value
-        };
-    } else if (object.datatype && object.datatype.toString() != 'http://www.w3.org/2001/XMLSchema#string') {
-        return {
-            '@type': object.datatype.toString(),
-            '@value': value
-        };
-    } else {
-        return value;
-    }
-}
-
-/**
- * Adds the key value to the given JSON-LD object.
- * @param object JSON-LD object
- * @param key the nominal value of the predicate
- * @param value the object value of the property as a JSON-LD object
- */
-function pushTriple(object, key, value) {
-    if (key === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type') {
-        if (typeof object['@type'] === 'undefined') {
-            object['@type'] = [];
-        }
-        object['@type'].push(value['@id']);
-    } else {
-        if (typeof object[key] === 'undefined') {
-            object[key] = value;
-        } else {
-            if (!Array.isArray(object[key])) {
-                object[key] = [object[key]];
-            }
-            object[key].push(value);
-        }
-    }
+    return ttl2JsonLd.rdfToJsonLd(parsedInput, parsedFrame, parsedAttrs);
 }
--- test/rdf-construct-mocha.js
+++ test/rdf-construct-mocha.js
@@ -1,20 +1,28 @@
 // rdf-construct-mocha.js
 
 var chai = require('chai');
+var expect = chai.expect;
 chai.should();
 chai.use(require('chai-as-promised'));
 
+
+var util = require('util');
+
 var http = require('http');
 var _ = require('underscore');
+
 var os = require('os');
 var fs = require('fs');
-var path = require('path');
+var request = require('request');
+
 var noflo = require('noflo');
+
 var test = require('./common-test');
 var stubs = require('./common-stubs');
 var rdfLoad = require('../components/rdf-load');
 var rdfNtriples = require('../components/rdf-ntriples');
 var requestTemplate = require('../components/request-template');
+
 
 describe('rdf-construct subgraph', function() {
     var port = 1337;
@@ -27,8 +35,10 @@ describe('rdf-construct subgraph', function() {
             "spouse": "http://dbpedia.org/resource/Cynthia_Lennon"
         }]
     };
-    it("should parse text/turtle", function() {
+
+    it("should parse text/turtle", function(done) {
         this.timeout(3250);
+
         var server = http.createServer();
         afterEach(_.once(server.close.bind(server)));
         server.on('request', function(req, res) {
@@ -40,20 +50,38 @@ describe('rdf-construct subgraph', function() {
             ].join('\n'));
         });
         server.listen(port);
+
         return test.createNetwork({
             construct: "rdf-components/rdf-construct",
             jsonld: "rdf-components/rdf-jsonld"
         }).then(function(network){
+
             network.graph.addEdge('construct', 'output', 'jsonld', 'input');
             network.graph.addInitial("http://localhost:" + port + "/", 'construct', 'sparql_endpoint');
+
             return new Promise(function(done) {
                 test.onOutPortData(network.processes.jsonld.component, 'output', done);
                 network.graph.addInitial("urn:test:graph", 'construct', 'source_graph_uri');
+
+            }).then(function(result) { 
+                result.should.be.an('object');
+                result.data.should.deep.equal([ 
+                    { '@id': 'http://dbpedia.org/resource/John_Lennon',
+                      'http://xmlns.com/foaf/0.1/name': 'John Lennon',
+                      'http://schema.org/spouse': { 
+                           '@id': 'http://dbpedia.org/resource/Cynthia_Lennon' } 
+                    } 
+                ]);
+                done();
+            }).catch(function(e) { 
+                throw Error('should parse text/turtle test exception: ',e);
             });
-        }).should.eventually.have.property('data').that.has.property('@id', "http://dbpedia.org/resource/John_Lennon");
+        });
     });
-    it("should parse text/turtle into JSON LD", function() {
+
+    it("should parse text/turtle into JSON LD", function(done) {
         this.timeout(3250);
+
         var server = http.createServer();
         afterEach(_.once(server.close.bind(server)));
         server.on('request', function(req, res) {
@@ -65,41 +93,113 @@ describe('rdf-construct subgraph', function() {
             ].join('\n'));
         });
         server.listen(port);
+
         return test.createNetwork({
             construct: "rdf-components/rdf-construct-jsonld"
         }).then(function(network){
             network.graph.addInitial("http://localhost:" + port + "/", 'construct', 'sparql_endpoint');
             return new Promise(function(done) {
                 test.onOutPortData(network.processes.construct.component, 'output', done);
+                var frame = { 
+                    "@context": {
+                        "_id": "@id",
+                        "id": "@id",
+                        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+                        "label": {
+                            "@id": "rdfs:label"
+                        }
+                    }
+                };
+                network.graph.addInitial(frame, 'construct', 'frame');
                 network.graph.addInitial("urn:test:graph", 'construct', 'source_graph_uri');
+
+            }).then(function(result) { 
+                result.should.be.an('object');
+                expect(result.error).to.be.undefined;
+                result.data.should.have.all.keys('@context', '@graph');
+                result.data.should.deep.equal(
+                    { '@context': 
+                         { _id: '@id',
+                           id: '@id',
+                           rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+                           label: { '@id': 'rdfs:label' } },
+                      '@graph': [
+                         { id: 'http://dbpedia.org/resource/Cynthia_Lennon' },
+                         { id: 'http://dbpedia.org/resource/John_Lennon',
+                           'http://schema.org/spouse': { 
+                               id: 'http://dbpedia.org/resource/Cynthia_Lennon' 
+                           },
+                           'http://xmlns.com/foaf/0.1/name': 'John Lennon' 
+                         } 
+                      ] 
+                    }
+                );
+                done();
             });
-        }).should.eventually.have.property('data').that.has.property('@id', "http://dbpedia.org/resource/John_Lennon");
+        });
     });
-    xit("should round trip jsonld through SPARQL service", function() {
-        this.timeout(3000);
-        if (!process.env['rdf-auth-file']) {
-            process.env['rdf-auth-file'] = path.join(os.tmpdir(), 'rdf-auth');
-        }
-        return test.createNetwork({
-            insert: "rdf-components/rdf-clear-insert-jsonld",
-            construct: "rdf-components/rdf-construct-jsonld"
-        }).then(function(network){
-            network.graph.addInitial('rdf-auth-file', 'insert', 'auth_file_env');
-            network.graph.addInitial('rdf-auth-file', 'construct', 'auth_file_env');
-            network.graph.addInitial(endpoint, 'insert', 'sparql_endpoint');
-            network.graph.addInitial(endpoint, 'construct', 'sparql_endpoint');
-            return stubs.promiseLater().then(function(){
-                return new Promise(function(done) {
-                    test.onOutPortData(network.processes.insert.component, 'output', done);
-                    network.graph.addInitial("urn:test:graph", 'insert', 'target_graph_uri');
-                    network.graph.addInitial(john, 'insert', 'parsed_jsonld');
-                });
-            }).then(stubs.promiseLater).then(function(){
-                return new Promise(function(done) {
-                    test.onOutPortData(network.processes.construct.component, 'output', done);
-                    network.graph.addInitial("urn:test:graph", 'construct', 'source_graph_uri');
-                });
-            });
-        }).should.eventually.have.property('data').that.has.property('@id', "http://dbpedia.org/resource/John_Lennon");
-    });
+
+    it("should round trip jsonld through SPARQL service", function(done) {
+        this.timeout(5000);
+
+        // Verify we have the rdf_auth_file env variable set so we have auth credentials
+        // Info on how to set auth credentials: 
+        if (!process.env['rdf_auth_file']) {
+            console.warn("    The \"round trip jsonld through SPARQL service\" test requires an RDF auth file path in env var \"rdf_auth_file\"");
+            console.warn("    Building an auth file is documented here: https://www.digitalocean.com/community/tutorials/how-to-set-up-ssh-keys--2");
+            console.warn("    Skipping the round trip jsonld through SPARQL service test!");
+            done();
+        } else { 
+
+            // Check whether the endpoint (which is on the VPN) is accessible 
+            // This test cannot be run if not on the VPN, so warn and move on if not.
+            request.get({ uri: endpoint, timeout: 3000}, function(error, response, body) {
+
+                if (!_.isEmpty(error)) { 
+                    console.warn("    Unable to access the Allegrograph endpoint.  Are you on the VPN?");
+                    console.warn("    The \"round trip jsonld through SPARQL service\" test requires VPN access to proceed.  Skipping this test.");
+                    done();
+                } else { 
+
+                    return test.createNetwork({
+                        insert: "rdf-components/rdf-clear-insert-jsonld",
+                        construct: "rdf-components/rdf-construct-jsonld"
+        
+                    }).then(function(network){
+        
+                        network.graph.addInitial('rdf_auth_file', 'insert', 'auth_file_env');
+                        network.graph.addInitial('rdf_auth_file', 'construct', 'auth_file_env');
+                        network.graph.addInitial(endpoint, 'insert', 'sparql_endpoint');
+                        network.graph.addInitial(endpoint, 'construct', 'sparql_endpoint');
+
+                        return stubs.promiseLater().then(function(){
+                            return new Promise(function(done) {
+                                test.onOutPortData(network.processes.insert.component, 'output', done);
+                                network.graph.addInitial("urn:test:graph", 'insert', 'target_graph_uri');
+                                network.graph.addInitial(john, 'insert', 'parsed_jsonld');
+                            });
+                        }).then(stubs.promiseLater).then(function(){
+
+                            return new Promise(function(done) {
+                                test.onOutPortData(network.processes.construct.component, 'output', done);
+                                network.graph.addInitial("urn:test:graph", 'construct', 'source_graph_uri');
+                            }).then(function(roundtripResult) {
+                                expect(roundtripResult.error).to.be.undefined;
+                                roundtripResult.data.should.be.an('array');
+                                roundtripResult.data.should.have.length(1);
+                                roundtripResult.data.should.deep.equal([ 
+                                    { '@id': 'http://dbpedia.org/resource/John_Lennon',
+                                      'http://schema.org/spouse': { 
+                                           '@id': 'http://dbpedia.org/resource/Cynthia_Lennon' 
+                                      },
+                                      'http://xmlns.com/foaf/0.1/name': 'John Lennon' } 
+                                ]);
+                                done();
+                            });
+                        });
+                    });
+                }
+           });
+       }
+   });
 });
